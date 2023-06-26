b4ba3ff32bad4b13fcf97237ddf13ba989edb236
sojeonglee <sojeonglee0125@gmail.com>
2023-06-26 20:34:38 +0900

    feat: 닉네임과 일반 메세지 타입 구분 추가

--- src/server.js
+++ src/server.js
@@ -15,16 +15,30 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log('Connected to the Browser!!');
     socket.on("close", () => console.log("Disconnected from the Browser!!"));
-    
-    socket.on("message", message => {
-        console.log(message.toString('utf8'));
+    socket.on("message", (message) => {
+        const messageString = message.toString('utf8');
+        sockets.forEach(aSocket => aSocket.send(messageString));
     });
-
-    socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
+
+
+{
+    type: "message",
+    payload: "hello"
+
+}
+
+{
+    type: "nickname",
+    payload: "js"
+
+}
 
