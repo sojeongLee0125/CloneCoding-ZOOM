3877d63d7d6e611cc7a9cca4c2989370a077ed89
sojeonglee <sojeonglee0125@gmail.com>
2023-06-26 19:21:09 +0900

    feat: 서버 - 브라우저 메세지 전송 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () =>{
+    console.log("Connected to the Server!!");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Got Messege : ", message.data);
+});
+
+socket.addEventListener("close", () =>{
+    console.log("Disconnected from the Server!!");
+});
+
+setTimeout(() => {
+    socket.send("hello from the Browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -15,11 +15,16 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-    console.log(socket);
-}
+wss.on("connection", (socket) => {
+    console.log('Connected to the Browser!!');
+    socket.on("close", () => console.log("Disconnected from the Browser!!"));
+    
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });
 
-wss.on("connection", handleConnection);
+    socket.send('hello!!!');
+});
 
 server.listen(3000, handleListen);
 
