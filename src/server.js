9ad012d675a5dc7d8ac5b0783ea2f93b8dbc74a4
sojeonglee <sojeonglee0125@gmail.com>
2023-06-28 10:41:48 +0900

    feat: 채팅방 생성 구현 완료

--- src/server.js
+++ src/server.js
@@ -16,12 +16,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) =>{
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
-        setTimeout(() =>{
-            done();
-        }, 5000);
-
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
     });
 });
 
