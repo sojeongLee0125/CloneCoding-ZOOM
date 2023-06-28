3e640de3b5bcb51eb65bfefeffe9f22b3d90b21e
sojeonglee <sojeonglee0125@gmail.com>
2023-06-28 19:46:06 +0900

    feat: 현재 활성 채팅방 리스트 보여주기 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -62,3 +62,18 @@ socket.on("bye", (left) => {
 socket.on("new_message", (msg) => {
     addMessage(msg);
 });
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if (rooms.length === 0) {
+        return;
+    }
+    
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    })
+});
+
--- src/server.js
+++ src/server.js
@@ -15,10 +15,29 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+
+    const publicRooms = [];
+    
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) =>{
     socket["nickname"] = "Anonymous";
 
     socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
         console.log(`Socket Event: ${event}`);
     });
     
@@ -26,10 +45,15 @@ wsServer.on("connection", (socket) =>{
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
 
     socket.on("disconnecting", () => {
         socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+    });
+
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
     });
 
     socket.on("new_message", (msg, room, done) => {
