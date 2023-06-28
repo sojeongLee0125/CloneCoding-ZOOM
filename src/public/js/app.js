882edec95ffbd31bf5ff5a0093f13b0a36816730
sojeonglee <sojeonglee0125@gmail.com>
2023-06-28 22:00:42 +0900

    feat: 기본 화상채팅 구현 1차 완료

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,20 +1,53 @@
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if (currentCamera.label === camera.label) {
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio: true,
+        video: { facingMode: "user" },
+    };
+
+    const cameraConstrains = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    };
+
     try{
         myStream = await navigator.mediaDevices.getUserMedia(
-            {
-                audio: true,
-                video: true,
-            }
+            deviceId? cameraConstrains : initialConstrains
         );
         myFace.srcObject = myStream;
+
+        if (!deviceId) {
+            await getCameras();
+        }
+
     }catch(e){
         console.log(e);
     }
@@ -23,6 +56,7 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
     if(!muted){
         muteBtn.innerText = "음소거 해제";
         muted = true;
@@ -33,6 +67,7 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
     if(cameraOff){
         cameraBtn.innerText = "카메라 끄기";
         cameraOff = false;
@@ -43,6 +78,12 @@ function handleCameraClick() {
     
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
 
